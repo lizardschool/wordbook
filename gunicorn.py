42ad6ad702e18c4d3e1f2a1d4df08aa0ab5cf706
matee <matee@matee.net>
2015-04-28 22:32:22 +0200

    guni

--- gunicorn.py
+++ gunicorn.py
@@ -1,12 +1,12 @@
 arbiter = "egg:gunicorn"   # The arbiter to use for worker management
 backlog = 512              # The listen queue size for the server socket
-bind = "unix:/var/run/wordbook.sock"     # Or "unix:/tmp/gunicorn.sock"
+bind = "unix:/home/apps/var/run/wordbook.sock"     # Or "unix:/tmp/gunicorn.sock"
 daemon = False            # Whether work in the background
 debug = True              # Some extra logging
 keepalive = 5             # Time we wait for next connection (in seconds)
-logfile = '/var/log/wordbook-gunicorn.log'               # Name of the log file
+logfile = '/home/apps/var/log/wordbook-gunicorn.log'               # Name of the log file
 loglevel = "info"         # The level at which to log
-pidfile = '/var/run/wordbook-gunicorn.pid'              # Path to a PID file
+pidfile = '/home/apps/var/run/wordbook-gunicorn.pid'              # Path to a PID file
 workers = 2               # Number of workers to initialize
 umask = 0                 # Umask to set when daemonizing
 user = 'apps'                 # Change process owner to user
