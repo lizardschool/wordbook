9640ff3db762f2d5d85363e61934c35e02c58aad
matee <matee@matee.net>
2015-08-07 12:00:58 +0200

    better test_add_translation

--- tests/flaskapp/test_ajax_views.py
+++ tests/flaskapp/test_ajax_views.py
@@ -1,4 +1,6 @@
 from flask import url_for
+from wordbook.domain.repo.translation import Repo
+from wordbook.infra.models.translation import Translation
 
 
 def test_add_translation(app, client):
@@ -15,3 +17,10 @@ def test_add_translation(app, client):
         headers={'Content-Type': 'application/x-www-form-urlencoded'})
     assert resp.status_code == 200
     assert resp.json == dict(translation=dict(id=1, ipa='sziip', translation='owca', word='sheep'))
+    repo = Repo()
+    translation = repo.get(resp.json['translation']['id'])
+    assert translation.translated == 'owca'
+    assert translation.from_language == 'en'
+    assert translation.word == 'sheep'
+    assert translation.ipa == 'sziip'
+    assert translation.into_language == 'pl'
