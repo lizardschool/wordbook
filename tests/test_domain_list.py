a7a3496eb52489f0ab50230f3d630c90072b4cd7
matee <matee@matee.net>
2015-08-09 23:49:27 +0200

    test_cardlist_foreign_language_name was moved from cardlist repo to cardlist domain model tests

--- tests/test_domain_list.py
+++ tests/test_domain_list.py
@@ -7,3 +7,15 @@ def test_list_hash():
 
     li.id = 2
     assert li.hash == 'oLEWq'
+
+
+def test_cardlist_foreign_language_name():
+    """Test returning cardlist foreign language iso code to name."""
+    li = List(
+        name='test1',
+        foreign_language='de',
+        known_language='pl',
+        definitions=True,
+        pictorials=True,
+    )
+    assert li.foreign_language_name == 'German'
